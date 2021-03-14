81269166af0009d58b1fb89cc96c3db846f9b37b
akmred <akmred@gmail.com>
2021-03-14 14:14:53 +0300

    first task

---
+++ app/src/main/java/com/example/noteskotlintwo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.noteskotlintwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        viewModel.viewState().observe(this, Observer {str ->
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+        })
+
+    }
+}
---
+++ app/src/main/java/com/example/noteskotlintwo/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.noteskotlintwo
+
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    private val viewStateLiveData = MutableLiveData<String>()
+
+    private val model = Model()
+
+    init {
+
+        model.getStringLiveData().observeForever{str ->
+
+            viewStateLiveData.value  = str
+
+        }
+
+    }
+
+
+    fun viewState(): LiveData<String> = viewStateLiveData
+
+}
