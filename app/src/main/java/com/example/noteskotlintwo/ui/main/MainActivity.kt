16a0e9a62dba19a7a42ab28bd00b99ed242ff41b
akmred <akmred@gmail.com>
2021-03-21 22:32:37 +0300

    with error

--- app/src/main/java/com/example/noteskotlintwo/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/noteskotlintwo/ui/main/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.example.noteskotlintwo.ui.main
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.example.noteskotlintwo.R
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var viewModel: MainViewModel
 
+    lateinit var viewModel: MainViewModel
+    lateinit var adapter: MainAdapter
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(findViewById(R.id.toolbar))//Не забудьте поменять в Стилях приложения тему на Theme.AppCompat.Light.NoActionBar
+
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        adapter = MainAdapter()
 
-        viewModel.viewState().observe(this, Observer {str ->
-            Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+        val main = findViewById<RecyclerView>(R.id.mainRecycler)
+        main.adapter = adapter
+
+        viewModel.viewState().observe(this, Observer<MainViewState> { t ->
+            t?.let { adapter.notes = it.notes }
         })
-
     }
 }
